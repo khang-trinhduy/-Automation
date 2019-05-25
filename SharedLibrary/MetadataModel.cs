bcc89e9d9a1f35e9e5398fb717cf359800cb44b6
Trịnh Duy Khang <khang.trinh@bankexpress.vn>
2019-05-25 18:17:20 +0700

    tweak metadata

--- SharedLibrary/MetadataModel.cs
+++ SharedLibrary/MetadataModel.cs
@@ -1,10 +1,17 @@
+using System.Runtime.Serialization;
+
 namespace SharedLibrary
 {
+    [DataContract]
     public class MetadataModel
     {
+        [DataMember(Name="Id")]
         public int Id { get; set; }
+        [DataMember(Name="Field")]
         public string Field { get; set; }
+        [DataMember(Name="Type")]
         public string Type { get; set; }
+        [DataMember(Name="Table")]
         public string Table { get; set; }
     }
 }
